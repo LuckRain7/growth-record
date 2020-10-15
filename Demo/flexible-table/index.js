ba51c3dce0b1a338afbe783d6790a2518917fba1
LuckRain7 <763839938@qq.com>
2020-10-15 11:15:02 +0800

    update

---
+++ Demo/flexible-table/index.js
@@ -0,0 +1,17 @@
+let renderData = [
+  {
+    level: "一注",
+    category: [
+      {
+        name: "上架",
+        trouble: [
+          { name: "上架异常1", des: "上架异常1的描述" },
+          { name: "上架异常1", des: "上架异常1的描述" },
+          { name: "上架异常1", des: "上架异常1的描述" },
+        ],
+      },
+      { name: "现蕾" },
+    ],
+  },
+  { level: "二注", category: [{ name: "上架2" }, { name: "现蕾2" }] },
+];
